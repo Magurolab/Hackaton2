7900280d30e4e47acb411bb29696fcf5c35295a9
wit <poowis2011@hotmail.com>
2018-05-10 17:35:25 +0700

    add template

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,12 @@
 import firebase from 'firebase'
 
 const config = {
-  apiKey: 'AIzaSyDOhyQXZMYPxIDRhUL9R3ar5j-DnP8qlL0',
-  authDomain: 'simple-note-31073.firebaseapp.com',
-  databaseURL: 'https://simple-note-31073.firebaseio.com',
-  projectId: 'simple-note-31073',
-  storageBucket: 'simple-note-31073.appspot.com',
-  messagingSenderId: '668969723863'
+  apiKey: 'AIzaSyA8YBu0mev63vnBzIQiYy6M4G3Ehnt_mtQ',
+  authDomain: 'hackaton2-7d785.firebaseapp.com',
+  databaseURL: 'https://hackaton2-7d785.firebaseio.com',
+  projectId: 'hackaton2-7d785',
+  storageBucket: 'hackaton2-7d785.appspot.com',
+  messagingSenderId: '1048977951330'
 }
 
 firebase.initializeApp(config)
