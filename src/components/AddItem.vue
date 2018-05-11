1ae0f3287fe4073eff7657a2c50524bfb48a261f
Siripatsorn <pinky.siripatsorn@gmail.com>
2018-05-11 10:29:06 +0700

    added add item page

---
+++ src/components/AddItem.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-container fluid>
+    <v-layout row wrap>
+
+      <v-flex xs12 class="text-xs-center" mt-3>
+        <p>Add items</p>
+      </v-flex>
+
+
+    </v-layout>
+  </v-container>
+</template>
