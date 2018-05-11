0739290f7f74b9ab2c6167515116c067978df65e
wit <poowis2011@hotmail.com>
2018-05-11 20:41:10 +0700

    buy almost done

--- src/components/Buy.vue
+++ src/components/Buy.vue
@@ -48,14 +48,73 @@
   </v-layout>
 </template>
 <script>
+  import { auth, db } from '../firebase'
   export default {
-    data: () => ({
-      cards: [
-        { title: 'Pre-fab homes', src: '/static/doc-images/cards/house.jpg', flex: 6 },
-        { title: 'Favorite road trips', src: '/static/doc-images/cards/road.jpg', flex: 6 },
-        { title: 'Best airlines', src: '/static/doc-images/cards/plane.jpg', flex: 6 },
-        { title: 'Best airlines', src: '/static/doc-images/cards/plane.jpg', flex: 6 }
-      ]
-    })
+    data () {
+      return {
+        cards: [
+          { title: 'Pre-fab homes', src: '/static/doc-images/cards/house.jpg', flex: 6 },
+          { title: 'Favorite road trips', src: '/static/doc-images/cards/road.jpg', flex: 6 },
+          { title: 'Best airlines', src: '/static/doc-images/cards/plane.jpg', flex: 6 }],
+        posts: []
+      }
+    },
+    components: {
+      auth, db
+    },
+    computed: {
+      error () {
+        return this.$store.state.error
+      },
+      loading () {
+        return this.$store.state.loading
+      }
+    },
+    methods: {
+      getAllPosts: function () {
+        db.ref('/Posts/').once('value').then(function (snapshot) {
+          var postObject = snapshot.val()
+          // var keys = Object.keys(postObject)
+          // this.posts = postObject
+          // console.log(postObject)
+          var tmp = []
+          for (let key in postObject) {
+            tmp.push({
+              id: key,
+              category: postObject[key].category,
+              description: postObject[key].description,
+              name: postObject[key].name,
+              price: postObject[key].price,
+              url: postObject[key].url,
+              user: postObject[key].user
+            })
+          }
+          this.posts = tmp
+        })
+      }
+    },
+    beforeMount () {
+      this.getAllPosts()
+      // console.log(this.posts)
+    },
+    watch: {
+      // getPosts () {
+      //   let postsRef = db.ref('Posts/').orderByKey().limitToLast(100)
+      //   postsRef.on('child_added', snapshot => {
+      //     let note = {text: snapshot.val(), id: snapshot.id}
+      //     this.snapshot = [note].concat(this.snapshot)
+      //   })
+      // },
+      error (value) {
+        if (value) {
+          this.alert = true
+        }
+      },
+      alert (value) {
+        if (!value) {
+          this.$store.commit('setError', null)
+        }
+      }
+    }
   }
 </script>
