938358d59eb4986c55e0df74ed86ef529264012c
Siripatsorn <pinky.siripatsorn@gmail.com>
2018-05-11 19:26:36 +0700

    add template for buy page

--- src/components/Buy.vue
+++ src/components/Buy.vue
@@ -1,15 +1,61 @@
 <template>
-  <v-container fluid>
-    <v-parallax src="/static/material.jpg">
-      <v-layout row wrap>
-        <v-flex xs12 text-xs-center mt-5>
-          <h1 class="black">Buy</h1>
-        </v-flex>
-      </v-layout>
-    </v-parallax>
-  </v-container>
-</template>
+  <v-layout column>
+    <v-flex xs12 sm6 >
 
+      <v-card>
+        <v-container fluid grid-list-sm>
+
+          <v-layout row wrap>
+            <v-flex
+              v-for="card in cards"
+              v-bind="{ [`xs${card.flex}`]: true }"
+              :key="card.title"
+              xs4
+            >
+              <v-card>
+                <v-card-media
+                  :src="card.src"
+                  height="200px"
+                >
+                  <v-container fill-height fluid>
+                    <v-layout fill-height>
+                      <v-flex xs12 align-end flexbox>
+                        <span class="headline white--text" v-text="card.title"></span>
+                      </v-flex>
+                    </v-layout>
+                  </v-container>
+                </v-card-media>
+                <v-card-actions>
+                  <v-spacer></v-spacer>
+                  <v-btn icon>
+                    <v-icon>favorite</v-icon>
+                  </v-btn>
+                  <v-btn icon>
+                    <v-icon>bookmark</v-icon>
+                  </v-btn>
+                  <v-btn icon>
+                    <v-icon>share</v-icon>
+                  </v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-flex>
+          </v-layout>
+
+        </v-container>
+      </v-card>
+
+    </v-flex>
+  </v-layout>
+</template>
 <script>
-  export default {}
+  export default {
+    data: () => ({
+      cards: [
+        { title: 'Pre-fab homes', src: '/static/doc-images/cards/house.jpg', flex: 6 },
+        { title: 'Favorite road trips', src: '/static/doc-images/cards/road.jpg', flex: 6 },
+        { title: 'Best airlines', src: '/static/doc-images/cards/plane.jpg', flex: 6 },
+        { title: 'Best airlines', src: '/static/doc-images/cards/plane.jpg', flex: 6 }
+      ]
+    })
+  }
 </script>
