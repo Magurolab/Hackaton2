4890fd2ff4b10d1c86009f28ae62a0bcc4473a51
wit <poowis2011@hotmail.com>
2018-05-16 21:24:57 +0700

    home show users items and can remove item

--- src/components/User/Home.vue
+++ src/components/User/Home.vue
@@ -1,56 +1,67 @@
 <template>
-  <v-container fluid>
-    <!--<v-flex xs12 class="text-xs-center" mt-5 v-if="loading">-->
-      <!--<v-progress-circular :size="70" :width="7" indeterminate color="purple"></v-progress-circular>-->
-    <!--</v-flex>-->
-
-    <v-layout row wrap>
-              <!--v-if="!loading"-->
-      <v-flex xs12 class="text-xs-center" mt-5>
-        <h1>Home page</h1>
-      </v-flex>
-      <v-flex xs12 sm6 offset-sm3>
-      <v-card>
-        <v-card-media src="/static/material.jpg" height="200px">
-        </v-card-media>
-        <v-card-title primary-title>
-          <div>
-            <h3 class="headline mb-0">Kangaroo Valley Safari</h3>
-            <div>{{email}}</div>
-            <div>{{this.$store.getters.getUserUniversity}}</div>
-            <v-btn flat color="orange" v-if="this.$store.getters.getUserUniversity==null">Loading</v-btn>
-          </div>
-        </v-card-title>
-        <v-card-actions>
-          <v-btn flat color="orange">Share</v-btn>
-          <v-btn flat color="orange">Explore</v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-flex>
-    </v-layout>
-  </v-container>
+  <v-flex xs12>
+    <v-card >
+      <v-container v-bind="{ [`grid-list-lg`]: true }" fluid>
+        <v-layout row wrap>
+          <v-flex
+            v-for=" card in cards"
+            :key="card.id"
+            xs12 sm6  md3 lg3 mt-3 >
+            <v-card-media
+              :src= "card.url"
+              height="300px"
+              style="cursor: pointer"
+            >
+            </v-card-media>
+            <v-card-actions>
+              <div  xs12 align-end flexbox>
+                <h3> {{card.name}}</h3>
+              </div>
+              <v-spacer></v-spacer>
+              <div>
+                {{'฿ ' + card.price}}
+              </div>
+              <v-btn icon
+                     @click="deleteItem(card.id)"
+              >
+                <v-icon>delete</v-icon>
+              </v-btn>
+            </v-card-actions>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-card>
+  </v-flex>
 </template>
 
 <script>
+  import { auth, db } from '../../firebase'
   export default {
-    data () {
-      return {
-        email: this.$store.state.user.email,
-        university: this.$store.getters.getUserUniversity
+    components: {
+      auth, db
+    },
+    computed: {
+      error () {
+        return this.$store.state.error
+      },
+      loading () {
+        return this.$store.state.loading
+      },
+      cards () {
+        return this.$store.getters.getCards.filter(function (u) {
+          return u.user === auth.currentUser.uid
+        })
       }
     },
     methods: {
-      // getUniversity () {
-      //   return this.$store.getters.getDatabaseValue('/university')
-      // }
-    },
-    computed: {
-      loading () {
-        return this.$store.getters.getUserUniversity == null
-      },
-      error () {
-        return this.$store.state.error
+      deleteItem (id) {
+        db.ref('Posts/').child(id).remove()
+        this.$store.dispatch('loadCards')
+        this.$router.push('/home')
       }
+    },
+    beforeMount () {
+      this.$store.dispatch('loadCards')
     },
     watch: {
       error (value) {
@@ -66,3 +77,14 @@
     }
   }
 </script>
+
+<style scoped>
+  .title {
+    position: absolute;
+    bottom: 5px;
+    color: white;
+    font-size: 2em;
+    margin-left: 5px;
+  }
+</style>
+
